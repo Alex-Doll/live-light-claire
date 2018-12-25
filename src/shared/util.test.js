a6696f24eccdac3f6e3ddfd75015e2bb8fe196f4
rosco1010 <doll3098@ou.edu>
2018-12-25 16:43:41 -0600

    Create tests for the utils in src/shared

--- src/shared/util.test.js
+++ src/shared/util.test.js
@@ -1,10 +1,27 @@
-import { isProd, scriptSource } from './util';
-import { WDS_PORT } from './config';
+import { WDS_PORT, STATIC_PATH } from './config';
 
-test('Should show false', () => {
+function getUtil(environment, utilAttribute) {
+  process.env.NODE_ENV = environment;
+  jest.resetModules();
+  return require('./util')[utilAttribute];
+}
+
+test('When in development, should show false', () => {
+  const isProd = getUtil('development', 'isProd');
   expect(isProd).toBe(false);
 });
 
+test('When in production, should show true', () => {
+  const isProd = getUtil('production', 'isProd');
+  expect(isProd).toBe(true);
+});
+
 test('Should be our Webpack Dev Server route', () => {
+  const scriptSource = getUtil('development', 'scriptSource');
   expect(scriptSource).toBe(`http://localhost:${WDS_PORT}/dist/js/bundle.js`);
+});
+
+test('Should be our STATIC_PATH route', () => {
+  const scriptSource = getUtil('production', 'scriptSource');
+  expect(scriptSource).toBe(STATIC_PATH + '/js/bundle.js');
 });
